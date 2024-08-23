89b11126d9f3cbabc948d6ca5f10828ef9d45b21
SaiAkshay Sure <151056155+suresaiakshay@users.noreply.github.com>
2024-08-23 21:25:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//components
+import login from './account/components/login'
+
+function App() {
+  return (
+    <div>
+      <login/>
+    </div>
+  );
+}
+
+export default App;
